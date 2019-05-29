a482586d7db67841da91d027be815aee24949cf5
larsnedb <lars.nedberg@gmail.com>
2019-05-29 10:28:28 +0200

    Validation for template-driven forms

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,15 +3,24 @@ import {Router} from '@angular/router';
 import {AuthService} from './auth.service';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: [`
+    em {
+      float: right;
+      color: #E05C65;
+      padding-left: 10px
+    }
+  `]
 })
 
 
 export class LoginComponent {
   userName;
   password;
+  mouserOverLogin: boolean;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
   login(loginForm) {
     this.authService.loginUser(loginForm.userName, loginForm.password);
